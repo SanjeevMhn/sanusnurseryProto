77bc8ed6713d8f542a093e42f4ecfc36762e6026
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-11-16 18:08:52 +0545

    [ADDED]: added hero section

--- inc/navbarSecondary.php
+++ inc/navbarSecondary.php
@@ -1,41 +1,26 @@
-<nav class="secondary-nav d-flex align-items-center justify-content-center p-2 border border-dark border-top-0">
-    <div class="dropdown">
-        <button class="cat-btn btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</button>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="#" class="dropdown-item">Fruit</a>
+<nav class="secondary-nav border border-dark border-top-0">
+    <div class="container d-flex align-items-center justify-content-center">
+        <ul class="nav-list list-unstyled d-flex align-items-center fs-5 p-0 m-0 w-50">
+            <li class="list-item active flex-fill">
+                <a href="#" class="list-item p-3 d-flex justify-content-center align-items-center">
+                    <span class="title-text">Home</span>
+                </a>
             </li>
-            <li>
-                <a href="#" class="dropdown-item">Vegetable</a>
+            <li class="list-item flex-fill">
+                <a href="#" class="list-item p-3 d-flex justify-content-center align-items-center">
+                    <span class="title-text">Shop</span>
+                </a>
             </li>
-            <li>
-                <a href="#" class="dropdown-item">Tree</a>
+            <li class="list-item flex-fill">
+                <a href="#" class="list-item p-3 d-flex justify-content-center align-items-center">
+                    <span class="title-text">About</span>
+                </a>
             </li>
-            <li>
-                <a href="#" class="dropdown-item">Shrubs</a>
+            <li class="list-item flex-fill">
+                <a href="#" class="list-item p-3 d-flex justify-content-center align-items-center">
+                    <span class="title-text">Contact</span>
+                </a>
             </li>
         </ul>
     </div>
-    <ul class="nav-list list-unstyled d-flex align-items-center fs-6 p-0 m-0">
-        <li class="list-item active">
-            <a href="#" class="list-item px-3 py-1">
-                <span class="title-text">Home</span>
-            </a>
-        </li>
-        <li class="list-item">
-            <a href="#" class="list-item px-3 py-1">
-                <span class="title-text">Shop</span>
-            </a>
-        </li>
-        <li class="list-item">
-            <a href="#" class="list-item px-3 py-1">
-                <span class="title-text">About</span>
-            </a>
-        </li>
-        <li class="list-item">
-            <a href="#" class="list-item px-3 py-1">
-                <span class="title-text">Contact</span>
-            </a>
-        </li>
-    </ul>
 </nav>
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php include_once "./inc/header.php" ?>
 <?php include_once "./inc/navbar.php" ?>
 <?php include_once "./inc/navbarSecondary.php" ?>
+<section class="hero-section">
+    <!-- <div class="img-container">
+        <img src="./assets/images/hero.jpg" alt="">
+    </div> -->
+</section>
 <?php include_once "./inc/footer.php" ?>
